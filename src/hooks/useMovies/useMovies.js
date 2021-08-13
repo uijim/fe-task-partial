8e89c7fbcbf650c117cf35ace8fba44d945fdb1d
James Chapman <james@devdoneright.co.uk>
2021-08-13 09:44:36 +0100

    Implement sort by popularity

--- src/hooks/useMovies/useMovies.js
+++ src/hooks/useMovies/useMovies.js
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import { useQuery } from 'react-query';
 
 import { fetchMovies } from '../../api';
@@ -5,10 +6,23 @@ import { fetchMovies } from '../../api';
 export const useMovies = () => {
   const { data = [], loading, error} = useQuery('movies', fetchMovies);
 
-  console.log(data);
+  const sortedData = useMemo(() => {
+    if (!data.length) return [];
+
+    return data.sort((a, b) => {
+      // we could just use lodash sorting but that could be seen as cheating ;)
+      const popularityA = Number.parseFloat(a.popularity);
+      const popularityB = Number.parseFloat(b.popularity);
+
+      if (popularityA > popularityB) return -1;
+      if (popularityA < popularityB) return 1;
+
+      return 0;
+    })
+  }, [data]);
 
   return {
-    data,
+    data: sortedData,
     error,
     loading
   }
